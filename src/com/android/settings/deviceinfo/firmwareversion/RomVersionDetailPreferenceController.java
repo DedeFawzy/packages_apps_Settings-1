4PERTURE/packages_apps_Settings

Settings: Add ROM Version to Firmware version window
[ Stallix - Evolution X]
* Adapted for Android 10

Change-Id: I97dfbb070bacc5b8bc4c03751bef9f319a883cbc

src/com/android/settings/deviceinfo/firmwareversion/RomVersionDetailPreferenceController.java
@@ -1,0 +1,68 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.SystemProperties;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+
+import androidx.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
+public class RomVersionDetailPreferenceController extends BasePreferenceController {
+
+    private static final Uri INTENT_URI_DATA = Uri.parse("https://paypal.me/joeyhuab/");
+    private static final String TAG = "romDialogCtrl";
+    private static final String ROM_PROPERTY = "org.evolution.version";
+    private final PackageManager mPackageManager = this.mContext.getPackageManager();
+
+    public RomVersionDetailPreferenceController(Context context, String preferenceKey) {
+        super(context, preferenceKey);
+    }
+
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    public CharSequence getSummary() {
+        String rom = SystemProperties.get(ROM_PROPERTY,
+                this.mContext.getString(R.string.device_info_default));
+        return rom;
+    }
+
+    public boolean handlePreferenceTreeClick(Preference preference) {
+        if (!TextUtils.equals(preference.getKey(), getPreferenceKey())) {
+            return false;
+        }
+        Intent intent = new Intent();
+        intent.setAction("android.intent.action.VIEW");
+        intent.setData(INTENT_URI_DATA);
+        if (this.mPackageManager.queryIntentActivities(intent, 0).isEmpty()) {
+            Log.w(TAG, "queryIntentActivities() returns empty");
+            return true;
+        }
+        this.mContext.startActivity(intent);
+        return true;
+    }
+}

DedeFawzy/packages_apps_Settings-1

Signed-off-by: SahilSOnar <sss.sonar2003@gmail.com>

src/com/android/settings/deviceinfo/firmwareversion/RomVersionDetailPreferenceController.java
@@ -1,0 +1,68 @@
+/*
+ * Copyright (C) 2019 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.SystemProperties;
+import android.text.TextUtils;
+import android.util.Log;
+import android.view.View;
+
+import androidx.preference.Preference;
+
+import com.android.settings.R;
+import com.android.settings.core.BasePreferenceController;
+
+public class RomVersionDetailPreferenceController extends BasePreferenceController {
+
+    private static final Uri INTENT_URI_DATA = Uri.parse("https://paypal.me/bunnyy95/");
+    private static final String TAG = "romDialogCtrl";
+    private static final String ROM_PROPERTY = "org.cesium.mod.version";
+    private final PackageManager mPackageManager = this.mContext.getPackageManager();
+
+    public RomVersionDetailPreferenceController(Context context, String preferenceKey) {
+        super(context, preferenceKey);
+    }
+
+    public int getAvailabilityStatus() {
+        return AVAILABLE;
+    }
+
+    public CharSequence getSummary() {
+        String rom = SystemProperties.get(ROM_PROPERTY,
+                this.mContext.getString(R.string.device_info_default));
+        return rom;
+    }
+
+    public boolean handlePreferenceTreeClick(Preference preference) {
+        if (!TextUtils.equals(preference.getKey(), getPreferenceKey())) {
+            return false;
+        }
+        Intent intent = new Intent();
+        intent.setAction("android.intent.action.VIEW");
+        intent.setData(INTENT_URI_DATA);
+        if (this.mPackageManager.queryIntentActivities(intent, 0).isEmpty()) {
+            Log.w(TAG, "queryIntentActivities() returns empty");
+            return true;
+        }
+        this.mContext.startActivity(intent);
+        return true;
+    }
+}